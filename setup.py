fa4d1585ef2d60073372f5e50026cbc44087fa63
rant-net <34341872+rand-net@users.noreply.github.com>
2021-03-28 15:01:57 +0530

    Bumps to version 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="js-dl",
-    version="0.0.1",
+    version="0.0.2",
     description="Download javascript libraries  from https://cdnjs.com",
     long_description=README,
     long_description_content_type="text/markdown",
